983ad9e6860875f9968e9a1f5caf4e02030d9966
abidinislak <abidinislak@gmail.com>
2022-10-13 05:08:04 +0300

    first of all

---
+++ src/test/java/com/all_in_one_springdata/springdata/SpringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all_in_one_springdata.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
