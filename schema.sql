5b30a0c288c003a859aeccd593ece3c1af8d384f
ABİDİN IŞLAK <abidinislak@jandarma.gov.tr>
2022-10-18 10:12:17 +0300

    initial for h-2 console required by mvc

---
+++ schema.sql
@@ -0,0 +1,8 @@
+--INSERT INTO springData.book (name)
+--VALUES ("asdasdas");
+--INSERT INTO springData.book (name)
+--VALUES ("java");
+--INSERT INTO springData.book (name)
+--VALUES ("c#");
+--INSERT INTO springData.book (name)
+--VALUES ("python");
--- target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO springData.book (name)
-VALUES ("asdasdas");
-INSERT INTO springData.book (name)
-VALUES ("java");
-INSERT INTO springData.book (name)
-VALUES ("c#");
-INSERT INTO springData.book (name)
-VALUES ("python");
